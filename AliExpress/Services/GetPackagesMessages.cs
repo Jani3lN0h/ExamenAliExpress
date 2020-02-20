eaa91e6bd15b11911bc9ba139809fad550bf587b
Janiel Baldemar Noh Celis <janiel.noh@blueoceantech.com.mx>
2020-02-20 15:55:18 -0600

    Comienzo de pruebas unitarias

--- AliExpress/Services/GetPackagesMessages.cs
+++ AliExpress/Services/GetPackagesMessages.cs
@@ -16,6 +16,14 @@ namespace AliExpress.Services
         private readonly ISetInfoDTOServices _setInfoDTOServices;
         private readonly ICalculateLowCost _calculateLowCost;
 
+        /// <summary>
+        /// Constructor de la clase GetPackagesMessages.
+        /// </summary>
+        /// <param name="getListPackagesServices">Dependencia de tipo IGetListPackagesServices.</param>
+        /// <param name="determineParcelFactory">Dependencia de tipo IDetermineParcelFactory.</param>
+        /// <param name="processMessagesServices">Dependencia de tipo IProcessMessagesServices.</param>
+        /// <param name="setInfoDTOServices">Dependencia de tipo ISetInfoDTOServices.</param>
+        /// <param name="calculateLowCost">Dependencia de tipo ICalculateLowCost</param>
         public GetPackagesMessages(IGetListPackagesServices getListPackagesServices, IDetermineParcelFactory determineParcelFactory, IProcessMessagesServices processMessagesServices, ISetInfoDTOServices setInfoDTOServices, ICalculateLowCost calculateLowCost)
         {
             _getListPackagesServices = getListPackagesServices ?? throw new ArgumentNullException(nameof(getListPackagesServices));
@@ -24,12 +32,28 @@ namespace AliExpress.Services
             _setInfoDTOServices = setInfoDTOServices ?? throw new ArgumentNullException(nameof(setInfoDTOServices));
             _calculateLowCost = calculateLowCost ?? throw new ArgumentNullException(nameof(calculateLowCost));
         }
+
+        /// <summary>
+        /// Método que se encargará de obtener el mensaje a mostrar.
+        /// </summary>
+        /// <param name="path">Ruta del archivo donde se obtiene la lista de paquetes.</param>
+        /// <param name="dtToday">Fecha con la que se está evaluando el envío (Puede ser hoy).</param>
         public void GetMessage(string path, DateTime dtToday)
         {
+            if (string.IsNullOrWhiteSpace(path))
+            {
+                throw new ArgumentNullException(nameof(path));
+            }
+
             List<IPackage> lstPackages = _getListPackagesServices.GetListPackages(path);
             CalculateMessagePackage(lstPackages, dtToday);
         }
 
+        /// <summary>
+        /// Método que calcula el mensaje a mostrar, dependiente si existe una paquetería o no.
+        /// </summary>
+        /// <param name="lstPackages">Lista de paquetes que se obtienen del archivo csv.</param>
+        /// <param name="dtToday">Fecha con la cual se hace la comparación del envío.</param>
         private void CalculateMessagePackage(List<IPackage> lstPackages, DateTime dtToday)
         {
             foreach (IPackage item in lstPackages)
@@ -58,6 +82,11 @@ namespace AliExpress.Services
             }
         }
 
+        /// <summary>
+        /// Método que se encarga de completar la información del DTO llamando al servicio.
+        /// </summary>
+        /// <param name="itemDTO">Objeto del DTO que se va a completar.</param>
+        /// <returns></returns>
         private IPackageInfoDTO SetInfoDTO(IPackageInfoDTO itemDTO)
         {
             _setInfoDTOServices.CompleteDTOInfo(itemDTO);
---
+++ AliExpressUTest/Services/GetPackagesMessagesUTests.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AliExpress.Services.UTests
+{
+    [TestClass()]
+    public class GetPackagesMessagesUTests
+    {
+        [TestMethod()]
+        public void GetMessageTest_PathEmpty_ArgumentNullException()
+        {
+            Assert.Fail();
+        }
+    }
+}
